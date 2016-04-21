28ca668c0174b43cb313b27df174158f0cebdd1f
Jerry Lin <jeroldlin@gmail.com>
2016-04-20 20:50:07 -0500

    added base for view change, fixed some bugs, added description for portfolio

--- WebDesign.js
+++ WebDesign.js
@@ -9,19 +9,57 @@ function initialPage(){
 	navbarAutoCollapse();
 }
 
+function modifyViews(){//to be continued
+	if (Cookies.get("view") == undefined){
+		if ($(window).width() > 768){
+			Cookies.set("view", "desktop");
+			prependViewportWidth("200");
+		}else{
+			Cookies.set("view", "mobile");
+			prependViewportWidth("700");
+		}
+	}else{
+		if (Cookies.get("view") == "desktop"){
+			prependViewportWidth("1200");
+		}else{
+			prependViewportWidth("700");
+		}
+	}
+	
+	$(document).ready(function(){
+		$(".changeView").click(function(){
+			if (Cookies.get("view") == "desktop"){
+				Cookies.set("view", "mobile");
+				location.reload();
+			}else{
+				Cookies.set("view", "desktop");
+				location.reload();
+			}
+		});
+	});
+	
+	function prependViewportWidth(width){
+		var metaTag=document.createElement('meta');
+		metaTag.name = "viewport"
+		metaTag.content = "width=" + width + ", initial-scale=5.0, maximum-scale=5.0, user-scalable=5"
+		document.getElementsByTagName('head')[0].appendChild(metaTag);
+	}
+};//to be continued
+
+
 function adjustUItoScreenSize(){
 	$(document).ready(function() {
 		if($( window ).width() > 768) {
 			toLargeScreen();
 		}
-		if($( window ).width() < 768) {
+		if($( window ).width() <= 768) {
 			toSmallScreen();
 		}
 		$(window).resize(function() {
 			if($( window ).width() > 768) {
 				toLargeScreen();
 			}
-			if($( window ).width() < 768) {
+			if($( window ).width() <= 768) {
 				toSmallScreen();
 			}
 		});
@@ -74,10 +112,11 @@ function slideMenu(){
 		tl.to(".navExpaned", 0, {left:"100%", opacity:0, height:0, top:"50%"});
 		tl.to(".navExpaned", 0.05, {left:"0%", opacity:"0.5", height:"0", top:"50%"});
 		tl.to(".navExpaned", 0.15, {left:0, opacity:1, height:"100%", top:"0"}, "-=0.1");
-		tl.to(".navExpaned", 0, {left:"100%", opacity:0, height:0, top:"50%"});
+		tl.to(".navExpaned", 0, {left:"100%", opacity:0, height:0, top:"50%"});+
 		tl.to(".navExpaned", 0.25, {left:"5%", opacity:"0.5", height:"30", top:"50%"});
 		tl.to(".navExpaned", 0.25, {left:"5%", opacity:"0", height:"0", top:"50%"});
 		tl.to("#bar3", 0.3, {left:0, opacity:0, height:"100%", top:"0", width:"200px"},"-=0.6");
+		tl.to("#bar3", 0.3, {left:0, opacity:0, height:"100%", top:"0", width:"0px"},"-=0.6");
 		tl.to(".navExpaned", 0.2, {left:0, opacity:1, height:"100%", top:"0"}, "-=0.2");
 		tl.to("#bar1", 0.05, {left:"50%", opacity:0, height:"0", top:"50%"}, "-=0.1");
 		tl.to("#bar1", 0.1, {left:"215px", opacity:1, height:"100%", top:"0"});
@@ -85,9 +124,10 @@ function slideMenu(){
 		tl.to("#bar2", 0.1, {left:"230px", opacity:1, height:"100%", top:"0"});
 		tl.to("#bar5", 0.05, {left:"50%", opacity:0, height:"0", top:"50%"}, "-=0.1");
 		tl.to("#bar5", 0.1, {left:"210px", opacity:1, height:"100%", top:"0"});
-		tl.from(".clickToPE", 0.2, {right:200, opacity:0, rotation: "-180"});
-		tl.from(".clickToLE", 0.2, {right:200, opacity:0, rotation: "-180"});
-		tl.from(".clickToCI", 0.2, {right:200, opacity:0, rotation: "-180"});
+		tl.from(".clickToPE", 0.15, {right:200, opacity:0, rotation: "-180"});
+		tl.from(".clickToLE", 0.15, {right:200, opacity:0, rotation: "-180"});
+		tl.from(".clickToCI", 0.15, {right:200, opacity:0, rotation: "-180"});
+		tl.from(".changeView", 0.15, {right:200, opacity:0, rotation: "-180"});
 	}else{
 		$("#welcome").delay(500).fadeIn(500);
 		tl.to(".navCollapsed", 0, {top:"1000px", opacity:"0"});
@@ -146,6 +186,7 @@ function navFunctions (){
 		tl.from(".clickToPE", 0.2, {right:200, opacity:0, rotation: "-180"});
 		tl.from(".clickToLE", 0.2, {right:200, opacity:0, rotation: "-180"});
 		tl.from(".clickToCI", 0.2, {right:200, opacity:0, rotation: "-180"});
+		tl.from(".changeView", 0.2, {right:200, opacity:0, rotation: "-180"});
 		bar = true;
 	}
 }
